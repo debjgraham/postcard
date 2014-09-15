d736082fe5fedf088781086e30763e64392700ec
Deborah Graham <debjgraham@gmail.com>
2014-09-15 19:43:13 -0400

    added a comment to have something to push for the second rev to github

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -27,6 +27,7 @@ class ViewController: UIViewController {
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
         // Code will evaluate when we press the button
+        // Comment to have to do a second GIT push
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         enterMessageTextField.text = ""
